044e8a1098d9c5d3fee79b09c633d7cfa0c2371b
mittalmona <mittalmona3003@gmail.com>
2024-07-30 16:37:27 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new typed(".text",{
+    Strings:["FRONTEND DEVELOPER" ," UI DESIGNER" , "POSTER DESIGNING" , "PROGRAMMER" , "WEB DEVELOPER" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
